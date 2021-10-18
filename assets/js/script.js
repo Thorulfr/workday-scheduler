b1c5b800e5d65de00e3b1601777ea93f4574e165
Ben H <deepfreespace@gmail.com>
2021-10-17 23:19:21 -0600

    Added text-editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,52 @@
 var DateTime = luxon.DateTime;
 
 // Get HTML elements so we can manipulate them
-var todayEl = document.querySelector('#currentDay');
+// var todayEl = $('#currentDay');
+
 
 // Variable declarations
 const today = DateTime.now().toLocaleString(DateTime.DATE_HUGE);
-const currentHour = DateTime.now().get('hour');
+// const currentHour = DateTime.now().get('hour');
+const currentHour = 12;
 
 // Display today's date
 function displayTodaysDate() {
-    todayEl.textContent = today;
+    $('#currentDay').text(today);
 }
 
 // Color-code hour blocks
 function colorCode() {
     // Loop through blocks
     for (let i = 9; i < 17; i++) {
-        var currentRowEl = document.getElementById(i);
+        var currentRowEl = $('#' + i);
         if (i < currentHour) {
-            currentRowEl.className += ' past'; 
+            currentRowEl.addClass('past'); 
         } else if (i == currentHour) {
-            currentRowEl.className += ' present'; 
+            currentRowEl.addClass('present'); 
         } else {
-            currentRowEl.className += ' future'; 
+            currentRowEl.addClass('future'); 
         }
     }
 }
+
+// Create 
+
+// BEGIN Listeners
+// Listen for clicks inside hour spans, then trigger input field
+$('.task-text').on ('click', 'span', function() {
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+// Listen for when user clicks out of editing hour span, then store text
+$('.task-text').on('blur', 'textarea', function() {
+    var text = $(this).val().trim();
+    var newSpan = $('<span>').text(text);
+    $(this).replaceWith(newSpan);
+    
+});
+// END Listeners
 
 displayTodaysDate();
 colorCode();
